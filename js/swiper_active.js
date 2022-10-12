02421c5f769dd1caf0d93687a2467d462cc65e4c
benji762 <112377860+benji762@users.noreply.github.com>
2022-10-12 13:20:16 +0900

    221012_2

--- js/swiper_active.js
+++ js/swiper_active.js
@@ -7,7 +7,10 @@ new Swiper('.s2_slider', {
 	spaceBetween : 30,
 	slidesPerGroup : 1,
 	freeMode: true,
-	
+	navigation : { // 네비게이션 설정
+		nextEl : '.swiper-button-next', // 다음 버튼 클래스명
+		prevEl : '.swiper-button-prev', // 이번 버튼 클래스명
+	},
 });
 
 })
