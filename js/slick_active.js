a5bc1e5504cc78400772658dc8a5e37cca5d93f1
benji762 <112377860+benji762@users.noreply.github.com>
2022-10-21 15:01:55 +0900

    221021_5

--- js/slick_active.js
+++ js/slick_active.js
@@ -3,6 +3,10 @@ $(document).ready(function() {
         lazyLoad: 'ondemand', // ondemand progressive anticipated
         infinite: true,
 		arrows : true,
-        dots : true
+        dots : true,
+        autoplay:true,
+        speed : 800,
+        autoplaySpeed : 2000, 
+        pauseOnHover : true
       });
 });
